676618508fbbed3909cc58b6e700bffa619b2859
EllaRan <64592073+EllaRan@users.noreply.github.com>
2026-01-06 14:22:11 +0200

    cleaned regression model

--- model_training/roberta_regression_model.py
+++ model_training/roberta_regression_model.py
@@ -6,43 +6,6 @@ from safetensors.torch import save_file, load_file
 import os
 
 
-class MeanVarianceLoss(nn.Module):
-    """
-    Loss implemented according to:
-    https://openaccess.thecvf.com/content_cvpr_2018/papers/Pan_Mean-Variance_Loss_for_CVPR_2018_paper.pdf
-    code mostly taken from:
-    https://github.com/Herosan163/AgeEstimation/blob/master/mean_variance_loss.py
-    """
-
-    def __init__(self, lambda_1, lambda_2, start_val, end_val):
-        super().__init__()
-        self.lambda_1 = lambda_1
-        self.lambda_2 = lambda_2
-        self.start_val = start_val
-        self.end_val = end_val
-
-    def forward(self, input, target):
-        target = target.type(torch.FloatTensor).cuda()
-        m = nn.Softmax(dim=1)
-        p = m(input)
-
-        # cross entropy loss
-        ce = nn.CrossEntropyLoss()
-        ce_loss = ce(input, torch.round(target).long())  # Rounding up the target to match the bins (tensor a)
-
-        # mean loss
-        a = torch.arange(self.start_val, self.end_val + 1, dtype=torch.float32).cuda()
-        mean = torch.squeeze((p * a).sum(1, keepdim=True), dim=1)
-        mse = (mean - target) ** 2
-        mean_loss = mse.mean() / 2.0
-
-        # variance loss
-        b = (a[None, :] - mean[:, None]) ** 2
-        variance_loss = (p * b).sum(1, keepdim=True).mean()
-
-        return ce_loss + self.lambda_1 * mean_loss + self.lambda_2 * variance_loss
-
-
 class RobertaForRegression(PreTrainedModel):
     """
     RoBERTa model for regression tasks using mean pooling.
@@ -66,18 +29,9 @@ class RobertaForRegression(PreTrainedModel):
         pooling_method = getattr(config, "pooling_method", "mean")
         num_attention_heads = getattr(config, "num_attention_heads", 8)
         loss = getattr(config, "loss", "mae")
-        lambda_1 = getattr(config, "lambda_1", 0.2)
-        lambda_2 = getattr(config, "lambda_2", 0.05)
-        start_val = getattr(config, "start_val", 0)
-        end_val = getattr(config, "end_val", 100)
 
         self.loss = loss
-        # Default lambdas chosen based on the original paper, used in mean variance loss
-        self.lambda_1 = lambda_1
-        self.lambda_2 = lambda_2
-        self.start_val = start_val
-        self.end_val = end_val
-        n_labels = end_val - start_val + 1 if loss == 'meanvar' else 1
+        n_labels = 1
         self.roberta = RobertaWithAdvancedPooling(model_name, num_labels=n_labels, pooling_method=pooling_method, dropout=dropout,
                                                   hidden_dim=hidden_dim, num_attention_heads=num_attention_heads, initialize_roberta=initialize_roberta)
         self.post_init()  # ensures weight init compatibility with HF
@@ -89,13 +43,11 @@ class RobertaForRegression(PreTrainedModel):
         loss = None
         if labels is not None:
             if self.loss == 'mae':
-                loss_fn = nn.L1Loss()  # this was the best so far, but for meanvar when relevant
+                loss_fn = nn.L1Loss()
             elif self.loss == 'mse':
                 loss_fn = nn.MSELoss()
             elif self.loss == 'huber':
                 loss_fn = nn.HuberLoss()
-            elif self.loss == 'meanvar':
-                loss_fn = MeanVarianceLoss(self.lambda_1, self.lambda_2, self.start_val, self.end_val)
             loss = loss_fn(logits, labels)
 
         return {'loss': loss, 'logits': logits}
@@ -126,13 +78,9 @@ class RobertaForRegression(PreTrainedModel):
         self.config.hidden_size = roberta_config.hidden_size
         self.config.dropout = float(self.roberta.dropout.p)
         self.config.num_attention_heads = int(self.roberta.num_attention_heads)
-        self.config.lambda_1 = float(self.lambda_1)
-        self.config.lambda_2 = float(self.lambda_2)
         self.config.loss = self.loss
         self.config.pooling_method = self.roberta.pooling_method
         self.config.hidden_dim = self.roberta.hidden_dim
-        self.config.start_val = int(self.start_val)
-        self.config.end_val = int(self.end_val)
         self.config.initialize_roberta = False  # don't reinitialize when loading
         self.config.dtype = "float32"  # Save dtype info
         self.config.save_pretrained(save_directory)
